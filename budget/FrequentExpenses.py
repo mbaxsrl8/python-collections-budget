e0c4b9e6efd7d2ac5077aab3e2b5f1edc0fb4fb7
RUI LU <a10115307@gmail.com>
2021-02-06 12:02:22 +0800

    Update FrequentExpenses.py
    fix variable name

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -16,10 +16,10 @@ spending_counter = collections.Counter(spending_categories)
 print(spending_counter)
 
 top5 = spending_counter.most_common(5)
-categries, count = zip(*top5)
+categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
-ax.bar(categries, count)
+ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
 
 plt.show()
